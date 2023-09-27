c75ed78bf9c470fbd28b682e9f5d583725611e48
Farouk Yombouna <tanou2340@gmail.com>
2023-09-27 22:12:35 +0200

    Responsive Sandwich menu - work on proress

--- news-homepage-main/main/main.js
+++ news-homepage-main/main/main.js
@@ -3,16 +3,25 @@ function toggleMenu() {
     const menuIcon = document.getElementById('menu-icon');
     const exitMenuIcon = document.getElementById('exit-menu-icon');
     const navList = document.querySelector('.header__nav .nav');
+    const sidePanel = document.getElementById('sidePanel');
+
 
     if (nav.classList.contains('header__nav--visible')) {
+        /* sidePanel.style.display = 'none'; */ // Hide the side panel
         nav.classList.remove('header__nav--visible');
         menuIcon.style.display = 'block';
         exitMenuIcon.style.display = 'none';
         navList.classList.remove('flex-column');
+
+        sidePanel.classList.remove('side-panel--visible'); // Hide the side panel
+        
     } else {
+        /* sidePanel.style.display = 'block'; */ // display the side panel
         nav.classList.add('header__nav--visible');
         menuIcon.style.display = 'none';
         exitMenuIcon.style.display = 'block';
         navList.classList.add('flex-column');
+
+        sidePanel.classList.add('side-panel--visible'); // Show the side panel
     }
 }
