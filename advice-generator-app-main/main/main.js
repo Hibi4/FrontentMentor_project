a720f64981aa1f61d0e39b9a8a590c1a04557c65
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-06 16:51:05 +0200

    Start working on advice generator project

---
+++ advice-generator-app-main/main/main.js
@@ -0,0 +1,18 @@
+/* const adviceId = document.querySelector(".advice-tag");
+const adviceQuote = document.querySelector(".advice-quote");
+const btn = document.querySelector(".btn");
+
+function generateAdvice() {
+  fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
+    .then((response) => response.json())
+    .then((response) => {
+      let data = response.slip;
+      let dataId = data.id;
+      let dataAdvice = data.advice;
+
+      adviceId.innerHTML = `advice # ${dataId}`;
+      adviceQuote.innerHTML = dataAdvice;
+    });
+    https://api.github.com/users
+} */ 
+
