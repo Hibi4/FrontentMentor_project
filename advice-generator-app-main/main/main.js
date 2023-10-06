70fca79bb8a9d02992abc12f117df6f5cb19996b
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-06 18:15:33 +0200

    Update readme file

--- advice-generator-app-main/main/main.js
+++ advice-generator-app-main/main/main.js
@@ -1,21 +1,3 @@
-/* const adviceId = document.querySelector(".advice-tag");
-const adviceQuote = document.querySelector(".advice-quote");
-const btn = document.querySelector(".btn");
-
-function generateAdvice() {
-  fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
-    .then((response) => response.json())
-    .then((response) => {
-      let data = response.slip;
-      let dataId = data.id;
-      let dataAdvice = data.advice;
-
-      adviceId.innerHTML = `advice # ${dataId}`;
-      adviceQuote.innerHTML = dataAdvice;
-    });
-    https://api.github.com/users
-} */ 
-
 const adviceId = document.getElementById("advice_number");
 const adviceQuote = document.getElementById("advice_text");
 
@@ -23,13 +5,17 @@ function generateAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
+      try {
+        console.log(response);
+        let id = response.slip.id;
+        let advice = response.slip.advice;
 
-      let id = response.slip.id;
-      let advice = response.slip.advice;
+        adviceId.innerHTML = id;
+        adviceQuote.innerHTML = `" ${advice} "`;
+      } catch (error) {
+        console.log(error);
+      }
 
-      adviceId.innerHTML = id;
-      adviceQuote.innerHTML = `" ${advice} "`; 
     })
 }
 
