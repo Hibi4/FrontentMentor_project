be423599ebb7b0fe86b46fb4e4c2d40bee81b47a
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-06 18:31:59 +0200

    responsive is (almost) finish

--- advice-generator-app-main/main/main.js
+++ advice-generator-app-main/main/main.js
@@ -6,7 +6,7 @@ function generateAdvice() {
     .then((response) => response.json())
     .then((response) => {
       try {
-        console.log(response);
+        // console.log(response);
         let id = response.slip.id;
         let advice = response.slip.advice;
 
