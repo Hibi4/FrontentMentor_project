0bbba72d620eca43f5184d195db6a8b4c6346e7b
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-06 17:55:03 +0200

    Challenge is almost finish

--- advice-generator-app-main/main/main.js
+++ advice-generator-app-main/main/main.js
@@ -16,3 +16,20 @@ function generateAdvice() {
     https://api.github.com/users
 } */ 
 
+const adviceId = document.getElementById("advice_number");
+const adviceQuote = document.getElementById("advice_text");
+
+function generateAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      let id = response.slip.id;
+      let advice = response.slip.advice;
+
+      adviceId.innerHTML = id;
+      adviceQuote.innerHTML = `" ${advice} "`; 
+    })
+}
+
