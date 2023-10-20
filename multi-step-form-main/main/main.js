80b393a257736cd1624430e1142db5ca25851c00
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-20 18:31:42 +0200

    Working on javascript showTab

--- multi-step-form-main/main/main.js
+++ multi-step-form-main/main/main.js
@@ -2,20 +2,22 @@ let currentTab = 0;
 showTab(currentTab);
 
 function showTab(n) {
-    let tabs = document.getElementsByClassName("tab");
+    var tabs = document.querySelectorAll(".tab");
+    console.log(tabs.length);
+    //let tabs = document.getElementsByClassName("tab");
     tabs[n].style.display = "block";
-    if (n == 0) {
-        document.getElementById("previous_btn").style.display = "none";
+    // document.getElementsByClassName("tab.info").style.display = "block";
+    if (n === 0) {
+        document.querySelector(".previous_btn").style.display = "none";
     } else {
-        document.getElementsByClassName("next_btn").style.display = "inline";
+        document.querySelector(".next_btn").style.display = "inline";
     }
     // show indicator here 
 }
 
 function nextPrev(n) {
-    let tabs = document.getElementsByClassName("info");
+    let tabs = document.getElementsByClassName("tab");
     tabs[currentTab].style.display = "none";
     currentTab = currentTab + n;
     showTab(currentTab);
-    
 }
