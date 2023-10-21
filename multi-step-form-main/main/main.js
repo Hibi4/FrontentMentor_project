73121d156d78f24ccc9459f594034911ebec71b6
Hibi4 <49773@etu.he2b.be>
2023-10-21 20:23:26 +0200

    Main JS bug is fixed

--- multi-step-form-main/main/main.js
+++ multi-step-form-main/main/main.js
@@ -1,9 +1,9 @@
 let currentTab = 0;
 showTab(currentTab);
 
-function showTab(n) {
+/* function showTab(n) {
     var tabs = document.querySelectorAll(".tab");
-    console.log(tabs.length);
+    console.log("currentvalue: ", n);
     //let tabs = document.getElementsByClassName("tab");
     tabs[n].style.display = "block";
     // document.getElementsByClassName("tab.info").style.display = "block";
@@ -13,6 +13,31 @@ function showTab(n) {
         document.querySelector(".next_btn").style.display = "inline";
     }
     // show indicator here 
+} */
+function showTab(n) {
+    let tabs = document.querySelectorAll(".tab");
+
+    for (const elt of tabs) {
+        elt.style.display = "none";
+    }
+
+    /* for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    } */ 
+    
+    tabs[n].style.display = "block";
+
+    if (n === 0) {
+        document.querySelector(".previous_btn").style.display = "none";
+    } else {
+        document.querySelector(".previous_btn").style.display = "inline";
+    }
+    
+    if (n === tabs.length - 1) {
+        document.querySelector(".next_btn").style.display = "none";
+    } else {
+        document.querySelector(".next_btn").style.display = "inline";
+    }
 }
 
 function nextPrev(n) {
