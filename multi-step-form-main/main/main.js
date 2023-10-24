758998ea48e1cde5c0ccfeab2d1b7d45a5132358
Diallo_Ousmane <49773@etu.he2b.be>
2023-10-24 15:14:10 +0200

    Change background color when input is empty

--- multi-step-form-main/main/main.js
+++ multi-step-form-main/main/main.js
@@ -36,20 +36,26 @@ function validateForm() {
     let x, y, i, valid = true;
     x = document.querySelectorAll(".tab");
     // x = document.getElementsByClassName("tab");
-    // y = x[currentTab].getElementsByTagName("input");
+    y = x[currentTab].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < y.length; i++) {
-      // If a field is empty...
-      if (y[i].value == "") {
+      // If a field is empty... isNaN(y[i].value
+      if(y[i].value == "") {
+        valid = false;
+        y[i].style.backgroundColor = "red";
+      }
+      /* if (y[i].value == "") {
         // add an "invalid" class to the field:
         // y[i].className += " invalid";
         // and set the current valid status to false
         valid = false;
-      }
+      } */ 
     }
+    
     // If the valid status is true, mark the step as finished and valid:
     /* if (valid) {
       document.getElementsByClassName("step")[currentTab].className += " finish";
     } */
+    console.log("Valeur de valid: "+valid);
     return valid; // return the valid status
   }
