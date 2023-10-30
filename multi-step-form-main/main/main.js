ee37318b0c45f25fd0ca1064f93d2cfa6464a386
Hibi4 <49773@etu.he2b.be>
2023-10-30 19:47:30 +0100

    Work on progress

--- multi-step-form-main/main/main.js
+++ multi-step-form-main/main/main.js
@@ -43,17 +43,17 @@ function validateForm() {
     // x = document.getElementsByClassName("tab");
     y = x[currentTab].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
+    /*for (i = 0; i < y.length; i++) {
       // If a field is empty... isNaN(y[i].value
       if(y[i].value == "" ) {
         valid = false;
         y[i].style.backgroundColor = "red";
       }
-    }
+    } */ 
 
     // If the valid status is true, mark the step as finished and valid:
     if (valid) {
-        let i, x = document.getElementsByClassName("step_div");
+        let x = document.getElementsByClassName("step_div");
     
         for (const elt of x) {
             elt.className = elt.className.replace(" finish", "");
@@ -82,9 +82,21 @@ function validateForm() {
             if (checkbox.checked) {
                 packageDiv.style.backgroundColor = "hsl(229, 24%, 87%)"; 
                 packageDiv.style.border = "solid 1px hsl(228, 45%, 44%)";
+                /* <div>
+                  <p id="">Online service</p>
+                  <p id="">Larger storage</p>
+                </div> */
                 array_addons.push(name);
                 array_addons.push(price);
                 console.log(array_addons);
+                for (const element of array_addons) {
+                    const divElt = document.createElement("div");
+                    const pElt = document.createElement("p");
+                    pElt.innerHTML = element
+                    divElt.appendChild(pElt);
+                    document.querySelector(".online_div").appendChild(divElt);
+                    // document.createElement("p").innerHTML = array_addons[index];
+                }
             } else {
                 packageDiv.style.backgroundColor = "rgb(246, 237, 237)";
                 packageDiv.style.border = "none";
@@ -115,29 +127,62 @@ function validateForm() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+    /* $(".monthly-billing").on("click", "button",function () {
+        let n = $("#arcade_article_name").text();
+        let p = $("#arcade_article_price").text();
+        let va = $(".article_name").text();
+        console.log("valeur de n: "+va);
+    }); */ 
     document.querySelector(".arcade_billing").addEventListener("click", function () {
         let n = document.getElementById("arcade_article_name").innerHTML;
         let p = document.getElementById("arcade_article_price").innerHTML;
         array_articles.push(n);
         array_articles.push(p);
+        document.querySelector(".arcade_billing").style.backgroundColor = "hsl(229, 24%, 87%)";
+        document.querySelector(".advanced_billing").style.backgroundColor = "white";
+        document.querySelector(".advanced_billing").style.backgroundColor = "white";
+        document.getElementById("selected_articles_names").innerHTML = n + " (Monthly)";
+        document.getElementById("selected_articles_prices").innerHTML = p;
         console.log(array_articles);
     });
 
     document.querySelector(".advanced_billing").addEventListener("click", function () {
         let n = document.getElementById("advanced_article_name").innerHTML;
-        let p = document.getElementById("advanced_article_price").innerHTML;   
+        let p = document.getElementById("advanced_article_price").innerHTML;
+        document.querySelector(".advanced_billing").style.backgroundColor = "hsl(229, 24%, 87%)";
+        document.querySelector(".pro_billing").style.backgroundColor = "white";
+        document.querySelector(".arcade_billing").style.backgroundColor = "white";
         array_articles.push(n);
         array_articles.push(p);
+        document.getElementById("selected_articles_names").innerHTML = n + " (Monthly)";
+        document.getElementById("selected_articles_prices").innerHTML = p;
         console.log(array_articles);
     });
 
     document.querySelector(".pro_billing").addEventListener("click", function () {
         let n = document.getElementById("pro_article_name").innerHTML;
         let p = document.getElementById("pro_article_price").innerHTML;
+        document.querySelector(".pro_billing").style.backgroundColor = "hsl(229, 24%, 87%)";
+        document.querySelector(".arcade_billing").style.backgroundColor = "white";
+        document.querySelector(".advanced_billing").style.backgroundColor = "white";
         array_articles.push(n);
         array_articles.push(p);
+        document.getElementById("selected_articles_names").innerHTML = n + " (Monthly)";
+        document.getElementById("selected_articles_prices").innerHTML = p;
         console.log(array_articles);
     });
     
 });
-  
+
+function displayArticles(list_article_) {
+    for (const elt of list_article_) {
+        document.getElementById("selected_articles_names").innerHTML = elt + "Monthly";
+        document.getElementById("selected_articles_prices").innerHTML = elt;
+    }
+}
+
+function displayAddons(list_addons_, name, price) {
+    for (const elt of list_addons_) {
+        
+    }
+}
