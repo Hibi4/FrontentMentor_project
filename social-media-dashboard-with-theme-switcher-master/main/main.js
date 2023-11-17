683ae20584083280874ca7631067ff2ec82f4018
Diallo_Ousmane <49773@etu.he2b.be>
2023-11-17 17:15:55 +0100

    Work on progress

--- social-media-dashboard-with-theme-switcher-master/main/main.js
+++ social-media-dashboard-with-theme-switcher-master/main/main.js
@@ -1,22 +1,22 @@
 const swtichTheme = document.querySelector(".switch");
+const switchInput = document.getElementById("input-switch");
+const header__title = document.querySelector(".header__title");
+const nb__likes = document.querySelector(".nb__likes");
 
-document.addEventListener("click", (e) => {
+/* document.addEventListener("click", (e) => {
     console.log("click on the switch icon");
-    /* let elt = document.body;
+    let elt = document.body;
     elt.classList.toggle("dark-mode");
     if (e.target.classList.contains("switch")) {
         document.body.classList.toggle("dark");
-    } */
-});
+    } 
+}); */
 
-/* function myfunction() {
-    let elt = document.body;
-    elt.classList.toggle("dark-mode");
-} */
-
-const switchInput = document.getElementById("input-switch");
 switchInput.addEventListener("click", (e) => {
     console.log("click on the switch input");
     let elt = document.body;
     elt.classList.toggle("dark-mode");
+    // header__title.classList.toggle(".header__title__dark");
+    header__title.style.color = "white";
+    nb__likes.style.color = "white";
 });
