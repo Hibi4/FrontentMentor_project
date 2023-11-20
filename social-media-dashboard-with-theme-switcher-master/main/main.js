9a161301db25085c472bcc5f0d589240116a867b
Hibi4 <49773@etu.he2b.be>
2023-11-20 18:32:19 +0100

    Work on project

--- social-media-dashboard-with-theme-switcher-master/main/main.js
+++ social-media-dashboard-with-theme-switcher-master/main/main.js
@@ -1,24 +1,69 @@
 const swtichTheme = document.querySelector(".switch");
 const switchInput = document.getElementById("input-switch");
 const header__title = document.querySelector(".header__title");
-const nb__likes = document.querySelector(".nb__likes");
-const dashboard__1__overview = document.querySelector(".dashboard__1__overview");
+const nb__likes = document.querySelectorAll(".nb__likes");
+const dashbord = document.querySelectorAll(".dashboard__1");
+const dashboard__1__overview = document.querySelectorAll(".dashboard__1__overview");
+const overviewText = document.querySelectorAll(".overview__text");
+const nbFollowers = document.querySelectorAll(".nb_followers");
 
-/* document.addEventListener("click", (e) => {
-    console.log("click on the switch icon");
-    let elt = document.body;
-    elt.classList.toggle("dark-mode");
-    if (e.target.classList.contains("switch")) {
-        document.body.classList.toggle("dark");
-    } 
-}); */
+// Get the position of the click relative to the center of the slider
+    /* var sliderWidth = event.currentTarget.offsetWidth;
+    var clickX = event.clientX - event.currentTarget.getBoundingClientRect().left;
+    var clickRelativeToCenter = clickX - sliderWidth / 2;
+  
+    // Determine if the click is on the left or right side of the slider
+    if (clickRelativeToCenter < 0) {
+      console.log('Clicked on the left side of the switch');
+    } else {
+      console.log('Clicked on the right side of the switch');
+    } */ 
+    // Get the position of the click relative to the center of the slider
 
-switchInput.addEventListener("click", (e) => {
+document.getElementById('input-switch').addEventListener('click', function(event) {
+    
+    var sliderWidth = this.offsetWidth;
+    var clickX = event.clientX - this.getBoundingClientRect().left;
+    var clickRelativeToCenter = clickX - sliderWidth / 2;
+
+    // Determine if the click is on the left or right side of the slider
+    if (clickRelativeToCenter < 0) {
+      console.log('Clicked on the left side of the switch');
+      overviewSection();
+    } else {
+      console.log('Clicked on the right side of the switch');
+    }
+});
+
+/* switchInput.addEventListener("click", (e) => {
     console.log("click on the switch input");
     let elt = document.body;
     elt.classList.toggle("dark-mode");
     // header__title.classList.toggle(".header__title__dark");
     header__title.style.color = "white";
-    nb__likes.style.color = "white";
-    dashboard__1__overview.style.backgroundColor = "hsl(230, 17%, 14%)";
-});
+    overviewSection();
+}); */
+
+
+function overviewSection() {
+    for(const elt of dashboard__1__overview) {
+        elt.style.backgroundColor = "hsl(230, 17%, 14%)";
+    }
+    // nb__likes.style.color = "white";
+    for (const elt of nb__likes) {
+        elt.style.color = "white";
+    }
+    for (const elt of overviewText) {
+        elt.style.color = "white";
+    }
+
+    for (const elt of dashbord) {
+        elt.style.backgroundColor = "hsl(230, 17%, 14%)";
+    }
+
+    for (const elt of nbFollowers) {
+        elt.style.color = "white";
+    }
+
+    
+}
