db6cfd361c7bab3652c29d42aa41ca9ff88142fe
Hibi4 <49773@etu.he2b.be>
2023-11-23 22:04:18 +0100

    Work on input's background color

--- social-media-dashboard-with-theme-switcher-master/main/main.js
+++ social-media-dashboard-with-theme-switcher-master/main/main.js
@@ -185,6 +185,53 @@ document.getElementById('input-switch').addEventListener('click', function(event
         applyLightMode();
     }
 });
+
+// Assuming your input element has an ID 'input-switch'
+// let inputSwitch = document.getElementById('input-switch');
+// inputSwitch.addEventListener('change', function() {
+// Get the input element
+// var input = document.querySelector("input");
+// Get the slider element
+// var slider = document.querySelector(".slider");
+// Change the background-color of the slider element when the input is clicked
+// input.addEventListener("click", function() {
+  // Use a random color for demonstration
+ //  var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  // Set the background-color of the slider element with style
+ //  slider.style.backgroundColor = randomColor;
+  // console.log("data change");
+//});
+
+
+
+// Add a change event listener to the input element
+/* inputSwitch.addEventListener('change', function() {
+    console.log("data change");
+    let styleSheet = document.styleSheets[0]; // Adjust the index if needed
+    let ruleIndex = getRuleIndex('.slider:before', styleSheet);
+*/
+    // Change the background color of the pseudo-element ':before'
+ //   styleSheet.cssRules[ruleIndex].style.backgroundColor = this.checked ? 'black' : 'red';
+    // Select the pseudo-element ':before' using the styleSheet API
+    //   var styleSheet = document.styleSheets[0]; // Adjust the index if needed
+    //  var ruleIndex = getRuleIndex('input:checked + .slider:before', styleSheet);
+
+    // Apply the transform style to the pseudo-element ':before'
+    /* if (ruleIndex !== -1) {
+        styleSheet.rules[ruleIndex].style.transform = this.checked ? 'translateX(26px)' : 'initial';
+    } */
+// });
+
+// Function to get the index of a rule in a stylesheet
+function getRuleIndex(selector, styleSheet) {
+  for (let i = 0; i < styleSheet.rules.length; i++) {
+    if (styleSheet.rules[i].selectorText === selector) {
+      return i;
+    }
+  }
+  return -1; // Rule not found
+}
+
 /* 
 
 
