8163457572230bcc1b8e6363d8e22636286dc37d
Diallo_Ousmane <49773@etu.he2b.be>
2023-11-28 18:17:12 +0100

    Work on progress - display data from json file to the screen

---
+++ rest-countries-api-with-color-theme-switcher-master/main/main.js
@@ -0,0 +1,38 @@
+const countries = document.querySelector('.countries');
+
+fetch('data.json')
+   .then(response => response.json())
+   .then(data => {
+        console.log(data);
+
+        for(const country of data) {
+            const div_country = document.createElement('div');
+            const img = document.createElement('img');
+            const divImg = document.createElement('div');
+            const divText = document.createElement('div');
+            img.src = country.flag;
+            img.alt = country.name;
+            img.className = 'country__div__img';
+            divImg.appendChild(img);
+            div_country.appendChild(divImg);
+
+            const h4 = document.createElement('h4'); 
+            h4.textContent = country.name; 
+            const population = document.createElement('p');
+            const region = document.createElement('p');
+            const capital = document.createElement('p');
+            population.textContent = 'Population: ' + country.population;
+            region.textContent = 'Region: ' + country.region;
+            capital.textContent = 'Capital: ' + country.capital;
+            divText.appendChild(h4);
+            divText.appendChild(population);
+            divText.appendChild(region);
+            divText.appendChild(capital);
+            div_country.appendChild(divText);
+            countries.appendChild(div_country);
+
+        }
+});
+
+ /* div_country.appendChild(divImg); 
+        div_country.appendChild(divText); */ 
