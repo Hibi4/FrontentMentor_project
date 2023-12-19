169c6c0b291bf405e26916d6ebfed10fbd91e66b
Diallo_Ousmane <49773@etu.he2b.be>
2023-12-19 18:39:32 +0100

    Work on progress - Show map with marker into the given coordinates

--- ip-address-tracker-master/main/main.js
+++ ip-address-tracker-master/main/main.js
@@ -1,6 +1,14 @@
-var map = L.map('map').setView([46.3630104, 2.9846608], 6);
+var map = L.map('map').setView([50.8655,4.3794], 6);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// marker icon 
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+
+L.marker([50.8655,4.3794]).addTo(map)
+
+// 50.7365,4.3808 leuven
+// 50.8655,4.3794 brussels
+// 46.3630104, 2.9846608 france
