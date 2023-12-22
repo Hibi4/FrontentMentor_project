f0b045a4b1aaf5e0afd9a7e98834ee4666708a90
Diallo_Ousmane <49773@etu.he2b.be>
2023-12-22 15:01:53 +0100

    Trying to locate a position on the map according the data from the API

--- ip-address-tracker-master/main/main.js
+++ ip-address-tracker-master/main/main.js
@@ -1,44 +1,99 @@
-var map = L.map('map').setView([50.8655,4.3794], 6);
+var map = L.map('map').setView([50.8655,4.3794], 6); 
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let ip__adress;
-let result = "";
+// let ip__adress;
 
 // marker icon 
 // var marker = L.marker([50.93404006958008, 4.327270030975342]).addTo(map);
 
 L.marker([50.93404006958008,4.327270030975342]).addTo(map)
 
+const geoIpifyUrl = "https://geo.ipify.org/api/v2/country";
+const geoIpifyKey = "";
+const ipStackUrl = "http://api.ipstack.com";
+const ipStackKey = "";
+
+// Define a function to get the IP address from geo.ipify API
+async function getIpFromGeoIpify() { 
+  const response = await fetch(`${geoIpifyUrl}?apiKey=${geoIpifyKey}`);
+  if(response.ok) { 
+    const data = await response.json();
+    document.getElementById("ip__address").innerHTML = data.ip;
+    document.getElementById("location").innerHTML = data.location.region+ " "+ data.location.country;
+    document.getElementById("timezone").innerHTML = "UTC "+data.location.timezone;
+    document.getElementById("isp").innerHTML = data.isp;
+    return data.ip; 
+  } else { 
+    throw new Error(`An error occurred: ${response.status}`);
+  }
+}
+
+// Define a function to get the location data from ipstack API
+async function getLocationFromIpStack(ip) { 
+  const response = await fetch(`${ipStackUrl}/${ip}?access_key=${ipStackKey}`);
+  if(response.ok) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error(`An error occurred: ${response.status}`);
+  }
+}
+
+// Define a function to use both APIs and log the results
+async function useBothApis() { 
+  try { 
+    const ip = await getIpFromGeoIpify(); 
+
+    const location = await getLocationFromIpStack(ip); 
+    console.log(`IP address: ${ip}`);
+    console.log(`Location: ${location.latitude}`);
+    // , ${location.region_name}, ${location.city}
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+useBothApis();
+
 // fetch('https://api.openweathermap.org/data/2.5/weather?q=Leuven&appid=2b5b0b7b0b0b0b0b0b0b0b0b0b0b0b0b')
 
 // https://geo.ipify.org/api/v2/country?apiKey=
 // http://api.ipstack.com/134.201.250.155?access_key=
-fetch('https://geo.ipify.org/api/v2/country?apiKey=')
+
+/* fetch('https://geo.ipify.org/api/v2/country?apiKey=')
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    getIP(data.ip);
-    // console.log("country: "+ip__adress);
+    document.getElementById("ip__address").innerHTML = data.ip;
+    document.getElementById("location").innerHTML = data.location.region+ " "+ data.location.country;
+    document.getElementById("timezone").innerHTML = "UTC "+data.location.timezone;
+    document.getElementById("isp").innerHTML = data.isp; */
+    
+    // getIpFromGeoIpify(data.ip);
     // var marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
     // marker.bindPopup("<b>Country: </b>" + data.location.country + "<br><b>City: </b>" + data.location.region).openPopup();
-});
+// });
 
-function getIP(ip) {
-    // return ip;
-    console.log("La valeur de l'ip: "+ip);
-    result = "http://api.ipstack.com/"+ip+"?access_key=";
-    console.log(result);
-    
-}
+/* function getIpFromGeoIpify(ip) {
+  console.log("ip from getIPGeoIpify function: "+ip);
+  ip__adress = ip;
+  console.log("ip__adress: "+ip__adress);
+} */ 
 
-fetch(result)
+// fetch('http://api.ipstack.com/'+ip__adress+'?access_key=8e4b0b0b0b0b0b0b0b0b0b0b0b0b0b0b')
+// ); 91.183.159.197
+// http://api.ipstack.com/${ip__adress}?apiKey=4594a60d80b4bed8993a7a1c8af3d1a4`
+// http://api.ipstack.com/134.201.250.155?access_key=
+/* fetch(`http://api.ipstack.com/${ip__adress}?access_key=4594a60d80b4bed8993a7a1c8af3d1a4`)
 .then(response => response.json())
 .then(data => {
-    console.log(data);
-});
+  console.log("data from ipstack");
+  console.log("latitude: "+data.latitude);
+  console.log("longitude: "+data.longitude);
+}) */
 
 /* // Define the API URLs and keys
 const geoIpifyUrl = "https://geo.ipify.org/api/v2/country";
