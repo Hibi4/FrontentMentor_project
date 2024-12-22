3f3b136832bef090571ff1cc1089a52eefced3ae
Ousmane Diallo <49773@etu.he2b.be>
2024-12-22 20:04:23 +0100

    Work on progress - improve CSS tags

--- product-list-with-cart-main/src/dessert.js
+++ product-list-with-cart-main/src/dessert.js
@@ -41,24 +41,9 @@ import waffleMobile from './assets/images/image-waffle-mobile.jpg';
 import waffleTablet from './assets/images/image-waffle-tablet.jpg';
 import waffleDesktop from './assets/images/image-waffle-desktop.jpg';
 
-
-const CartIcon = () => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-    >
-        <circle cx="8" cy="21" r="1" />
-        <circle cx="19" cy="21" r="1" />
-        <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
-    </svg>
-);
+const Carbon__neutral = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/></svg>
+)
 
 const DecrementButton = ({ onClick }) => (
     <button className='decrement__button' onClick={onClick}>
@@ -91,6 +76,15 @@ const EmptyCard = () => (
         <path fill="#AD8A85" d="M5.823 65.953c-.96 8.453 16.746 17.44 39.573 20.027 22.827 2.586 42.053-2.187 43.013-10.667L87.076 87.1c-.96 8.48-20.24 13.333-43.04 10.666C21.236 95.1 3.53 86.22 4.49 77.74l1.334-11.787Z" />
         <path fill="#CAAFA7" d="M60.836 42.78a119.963 119.963 0 0 0-10.347-1.627c-24-2.667-44.453 1.893-45.333 10.373l-2.133 18.88a3.556 3.556 0 1 0 7.066.8 3.574 3.574 0 1 1 7.094.8l-.8 7.094a5.93 5.93 0 1 0 11.786 1.333 3.556 3.556 0 0 1 7.067.8l-.267 2.347a3.573 3.573 0 0 0 7.094.826l.133-1.2a5.932 5.932 0 1 1 11.787 1.36l-.4 3.52a3.573 3.573 0 0 0 7.093.827l.933-8.267a1.174 1.174 0 0 1 1.307-.906 1.146 1.146 0 0 1 1.04 1.306 5.947 5.947 0 0 0 11.813 1.334l.534-4.72a3.556 3.556 0 0 1 7.066.8 3.573 3.573 0 0 0 7.094.826l1.786-15.546a2.373 2.373 0 0 0-2.08-2.667L44.143 55.74l16.693-12.96Z" />
         <path fill="#87635A" d="m59.156 57.66 1.68-14.88-16.827 13.173 15.147 1.707Z" /><path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width=".974" d="M9.796 52.06c-.667 5.866 16.24 12.586 37.733 15.04 14.774 1.68 27.867.906 34.854-1.654" />
+    </svg>
+)
+
+const Shop__icon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)">
+        <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
+        <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z" /></g><defs>
+            <clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z" />
+            </clipPath></defs>
     </svg>
 )
 
@@ -266,17 +260,6 @@ function Dessert() {
     const handleAddToCart = (id) => {
         setCount(count + 1)
 
-        /* setSelectedItems(prev => {
-            const newSet = new Set(prev)
-            if(newSet.has(id)) {
-                setCount(count - 1);
-                newSet.delete(id)
-            } else {
-                setCount(count + 1);
-                newSet.add(id)
-            }
-            return newSet
-        }) */
         setSelectedItems(prev => {
             const newSet = new Set(prev);
             newSet.add(id);
@@ -329,7 +312,8 @@ function Dessert() {
                                     {(!productCounts[dessert.id] || productCounts[dessert.id] === 0)
                                         ? (
                                             <button className="add__to__cart" onClick={() => handleAddToCart(dessert.id)}>
-                                                <CartIcon />
+                                                 {/* <CartIcon /> */ }
+                                                <Shop__icon /> 
                                                 Add to Cart
                                             </button>
                                         ) : (<button className="selected__item">
@@ -390,8 +374,19 @@ function Dessert() {
                         {Object.keys(productCounts).length > 0 && (
                             <div>
                                 <div className="cart-total">
-                                    <strong>Total: ${calculateTotal().toFixed(2)}</strong>
+                                    <div>
+                                        <p>Order Total</p>
+                                    </div>
+                                    <div>
+                                        <p> <strong>${calculateTotal().toFixed(2)}</strong> </p>
+                                    </div>
                                 </div>
+                                <div className='carbon__neutral__tag'>
+                                    <div>
+                                        <p> <Carbon__neutral /> This is a <strong>carbon-neutral</strong> delivery. </p>
+                                    </div>
+                                </div>
+
                                 <div id='confirm__order__btn'>
                                     <button onClick={openModal}>Confirm Order</button>
                                 </div>
