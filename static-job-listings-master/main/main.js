5620a9353524e0c3d2a65c706a528ba4a2b4363a
Hibi4 <49773@etu.he2b.be>
2025-04-13 19:31:06 +0200

    work on progress - continue working on frontend features (CSS & JS)

--- static-job-listings-master/main/main.js
+++ static-job-listings-master/main/main.js
@@ -21,6 +21,7 @@ function displayElemets(data) {
     for (const elt of data) {
 
         // create elements
+        const jobListing_div = document.createElement('div');
         const main_div = document.createElement('div');
         const second_div = document.createElement('div');
         const div_picture = document.createElement('div');
@@ -34,6 +35,7 @@ function displayElemets(data) {
         const logo_div = document.createElement('div');
         const languages_div = document.createElement('div');
         const contract_div = document.createElement('div');
+        const third_div = document.createElement('div');
 
         const span_company = document.createElement('span');
         const old = document.createElement('span');
@@ -41,20 +43,23 @@ function displayElemets(data) {
         const featured_text = document.createElement('span');
         const contract = document.createElement('span');
         const span_duration = document.createElement('span');
-        const span_position = document.createElement('span');
         const span_role = document.createElement('span');
         const span_day = document.createElement('span');
         const span_contract = document.createElement('span');
         const span_location = document.createElement('span');
         const logo = document.createElement('img');
+        const role_span = document.createElement('span');
+        const position_span = document.createElement('span');
 
         // styles 
+        jobListing_div.className = 'listing_div';
         main_div.className = 'divMain';
         profile_div.className = 'profile_div';
         div_picture.className = 'div_picture';
         second_div.className = 'second_div';
         div_job_title.className = 'div_job_title';
         contract_div.className = 'contract_div';
+        third_div.className = 'third_div';
         
         
         role_div.className = 'role_div';
@@ -63,14 +68,17 @@ function displayElemets(data) {
         logo_div.className = 'logo_div';
         span_contract.className = 'span_contract';
         languages_div.className = 'languages_div';
+        role_span.className = 'role_span';
+        position_span.className = 'position_span';
+
 
         // 
         span_company.textContent = elt.company;
         logo.src = elt.logo;
         job_title.textContent = elt.position;
         span_duration.textContent = elt.postedAt + ' - ' + elt.contract + ' - ' + elt.location;
-        // span_contract.textContent = elt.contract;
-        // old.textContent = elt.new;
+        role_span.textContent = elt.role;
+        position_span.textContent = elt.level;
         
         profile_div.appendChild(span_company);
         
@@ -81,15 +89,18 @@ function displayElemets(data) {
         } else {
             old.textContent = " ";
             profile_div.appendChild(old);
+            
         }
 
         if(elt.featured === true) {
             featured_text.textContent = ' Featured';
             profile_div.appendChild(featured_text);
-            
+            main_div.classList.add('featured-listing');
         } else {
             featured_text.textContent = ' ';
             profile_div.appendChild(featured_text);
+            // main_div.classList.add('featured-listing');
+            document.querySelectorAll('.featured-listing').display= 'none';
         }
 
         // profile_div.appendChild(old);
@@ -103,10 +114,31 @@ function displayElemets(data) {
         second_div.appendChild(div_job_title);
         second_div.appendChild(contract_div);
         second_div.appendChild(duration_div);
+        
+        third_div.appendChild(role_span);
+        third_div.appendChild(position_span);
+        
+        // Ajouter les langages
+        elt.languages.forEach(language => {
+            const language_span = document.createElement('span');
+            language_span.className = 'language_span';
+            language_span.textContent = language;
+            third_div.appendChild(language_span);
+        });
+        
+        // Ajouter également les outils de la même façon
+        elt.tools.forEach(tool => {
+            const tool_span = document.createElement('span');
+            tool_span.className = 'language_span';
+            tool_span.textContent = tool;
+            third_div.appendChild(tool_span);
+        });
+        
         main_div.appendChild(second_div);
-//        main_div.appendChild(logo);
-        // main_div.appendChild(profile_div);
-        main.appendChild(main_div);
+        main_div.appendChild(third_div);
+        
+        jobListing_div.appendChild(main_div);
+        main.appendChild(jobListing_div);
 
     }
 
