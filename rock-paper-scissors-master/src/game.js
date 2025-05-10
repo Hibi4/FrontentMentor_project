dc649d657591110ff2720cde6e90fae23a842e20
Hibi4 <49773@etu.he2b.be>
2025-05-10 22:10:45 +0200

    Work on project - Improve Rules button's design

--- rock-paper-scissors-master/src/game.js
+++ rock-paper-scissors-master/src/game.js
@@ -9,46 +9,49 @@ function Game() {
     const [showRules, setShowRules] = useState(false);
 
     return (
-        <>
-            <div className='main'>
-                <div className='header'>
-                    <div className='names-tag'>
-                        <p>Rock</p>
-                        <p>Paper</p>
-                        <p>Scissors</p>
-                    </div>
-                    <div className='score-tag'>
-                        <span>Score</span> <br />
-                        <span id='score'>12</span>
-                    </div>
-                </div>
-                
-                <div className='rule-btn'>
-                    <Button variant="outline-light" onClick={() => setShowRules(true)}>
-                        Règles
-                    </Button>
-                </div>
-            </div>
-            
-            <Modal
-                show={showRules}
-                onHide={() => setShowRules(false)}
-                centered
-            >
-                <Modal.Header className="d-flex justify-content-between align-items-center border-0">
-                    <Modal.Title>Règles du jeu</Modal.Title>
-                    <div> <img src={closeIcon} alt="close Icon" onClick={() => setShowRules(false)} style={{cursor : 'pointer'}} /> </div>
-                </Modal.Header>
-                <Modal.Body className="text-center">
-                    <img src={image_rules} alt="Règles du jeu" style={{maxWidth: '100%'}} />
-                </Modal.Body>
-                {/* <Modal.Footer>
+         <>
+              <div className='main'>
+                   <div className='header'>
+                        <div className='names-tag'>
+                             <p>Rock</p>
+                             <p>Paper</p>
+                             <p>Scissors</p>
+                        </div>
+                        <div className='score-tag'>
+                             <span>Score</span> <br />
+                             <span id='score'>12</span>
+                        </div>
+                   </div>
+
+                   <div className='rule-btn'>
+                        <Button variant="outline-light" onClick={() => setShowRules(true)} style={ { backgroundColor: 'transparent', color: 'white' } }>
+                             Règles
+                        </Button>
+                   </div>
+              </div>
+
+              <Modal
+                   show={showRules}
+                   onHide={() => setShowRules(false)}
+                   centered
+              >
+                   {/*
+                    className="d-flex justify-content-between align-items-center border-0" 
+               */ }
+                   <Modal.Header className="modal_header">
+                        <Modal.Title>Règles du jeu</Modal.Title>
+                        <div> <img src={closeIcon} alt="close Icon" onClick={() => setShowRules(false)} style={{ cursor: 'pointer' }} /> </div>
+                   </Modal.Header>
+                   <Modal.Body className="text-center">
+                        <img src={image_rules} alt="Règles du jeu" style={{ maxWidth: '100%' }} />
+                   </Modal.Body>
+                   {/* <Modal.Footer>
                     <Button variant="secondary" onClick={() => setShowRules(false)}>
                         Fermer
                     </Button>
                 </Modal.Footer>*/ }
-            </Modal>
-        </>
+              </Modal>
+         </>
     );
 }
 
