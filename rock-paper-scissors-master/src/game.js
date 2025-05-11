0c1574501487064fbae73dfc47e5ef279537d94b
Hibi4 <49773@etu.he2b.be>
2025-05-11 21:13:10 +0200

    Work on progress - Working on the frontend game area

--- rock-paper-scissors-master/src/game.js
+++ rock-paper-scissors-master/src/game.js
@@ -2,12 +2,21 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import './game.css';
+import bg_triangle from './images/bg-triangle.svg'
 import image_rules from './images/image-rules.svg';
 import closeIcon from './images/icon-close.svg'
+import paper from './images/icon-paper.svg'
+import scissors from './images/icon-scissors.svg'
+import rock from './images/icon-rock.svg'
 
 function Game() {
     const [showRules, setShowRules] = useState(false);
+    const [playerChoice, setPlayerChoice] = useState({});
 
+    const handleItemClick = (item) => {
+          console.log(`You choose ${item}`);
+          setPlayerChoice(item);
+    } 
     return (
          <>
               <div className='main'>
@@ -22,7 +31,56 @@ function Game() {
                              <span id='score'>12</span>
                         </div>
                    </div>
+                   {/* start main section page */}
+                   <div className='game-tag'>
+                        <div className='triangle-bg'>
+                             <img src={bg_triangle} alt='triangle_bg' className='triangle-img' />
 
+                             <div className='game-item paper-item' onClick={ () => handleItemClick('paper')}>
+                                  <div className='game-item-inner'>
+                                       <img src={paper} alt='paper' />
+                                  </div>
+                             </div>
+
+                             <div className='game-item scissors-item' onClick={() => handleItemClick('scissors')}>
+                                  <div className='game-item-inner'>
+                                       <img src={scissors} alt='scissors' />
+                                  </div>
+                             </div>
+
+                             <div className='game-item rock-item' onClick={() => handleItemClick('rock')}>
+                                  <div className='game-item-inner'>
+                                       <img src={rock} alt='rock' />
+                                  </div>
+                             </div>
+                        </div>
+                   </div>
+                   {/* end main section page */}
+
+                   {/* start game area*/ }
+                   <div className='game-area'>
+                        <div className='choices'>
+                             <div>
+                                  <h4>You picked</h4>
+                             </div>
+                             <div>
+                                  <h4>The house picked</h4>
+                             </div>
+
+                        </div>
+                        <div className='game-zone'>
+                             {/* start part for user's choice */}
+                             <div className='game-are-inner user-choice'>
+
+                             </div>
+                             <div className='game-are-inner computer-choice'>
+
+                             </div>
+                             {/* end part for user's choice */}
+                        </div>
+
+                   </div>
+                   {/* end game area*/ }
                    <div className='rule-btn'>
                         <Button variant="outline-light" onClick={() => setShowRules(true)} style={ { backgroundColor: 'transparent', color: 'white' } }>
                              Règles
