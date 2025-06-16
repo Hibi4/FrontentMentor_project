f62748db93a4241cba0eba2b87388a5635917427
Hibi4 <49773@etu.he2b.be>
2025-06-16 20:01:42 +0200

    Create and start project

---
+++ expenses-chart-component-main/src/chart.js
@@ -0,0 +1,29 @@
+import './chart.css'
+
+function Chart() {
+    return (
+        <>
+            <div id='body'>
+                <div id='root'>
+                    <div className='header'>
+                        <div>
+                            <p>My balance</p>
+                            <h2>$921.48</h2>
+                        </div>
+                        <div>
+                            <p>giebg ebvebj</p>
+                        </div>
+                        
+                    </div>
+                    <div className=''>
+                        <div className='revenue'></div>
+                    </div>
+                    {/* <h1>This the root div.</h1>*/ }
+                </div>
+            </div>
+        </>
+    )
+    
+}
+
+export default Chart;
